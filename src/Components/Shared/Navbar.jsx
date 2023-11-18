20a748e456a0c15dbaa2d0e87b3936701dba9d06
mdhossain07 <mrabir8097@gmail.com>
2023-11-18 12:11:46 +0600

    responsive navbar done

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 import logo from "../../../public/Doc House Logo.svg";
 import Container from "./Container";
+import { FaAlignJustify } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleMenu = () => {
+    setIsClicked(!isClicked);
+    console.log(isClicked);
+  };
   const navItems = (
-    <div className="flex gap-3">
+    <>
       <Link
+        style={{ marginRight: "20px" }}
         className={({ isActive }) => (isActive ? "bg-[#F7A582] p-3" : "")}
         to="/"
       >
@@ -13,6 +21,7 @@ const Navbar = () => {
       </Link>
 
       <Link
+        style={{ marginRight: "20px" }}
         className={({ isActive }) => (isActive ? "bg-[#F7A582] p-3" : "")}
         to="/"
       >
@@ -20,6 +29,7 @@ const Navbar = () => {
       </Link>
 
       <Link
+        style={{ marginRight: "20px" }}
         className={({ isActive }) => (isActive ? "bg-[#F7A582] p-3" : "")}
         to="/"
       >
@@ -27,12 +37,13 @@ const Navbar = () => {
       </Link>
 
       <Link
+        style={{ marginRight: "20px" }}
         className={({ isActive }) => (isActive ? "bg-[#F7A582] p-3" : "")}
         to="/"
       >
         Login
       </Link>
-    </div>
+    </>
   );
   return (
     <>
@@ -45,7 +56,23 @@ const Navbar = () => {
             </h2>
           </div>
 
-          <div className="font-medium">{navItems}</div>
+          <div className="font-medium hidden md:block lg:block">{navItems}</div>
+
+          <div className="">
+            <FaAlignJustify
+              onClick={handleMenu}
+              className="text-2xl cursor-pointer block md:hidden lg:hidden ml-16 text-white hover:text-green-400"
+            />
+            <div className="lg:hidden  transition relative">
+              <div
+                className={`flex flex-col gap-1 bg-white text-black absolute top-2 w-[200px] right-0 z-0 rounded-md p-3 ${
+                  isClicked ? "block" : "hidden"
+                }`}
+              >
+                {navItems}
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </>
